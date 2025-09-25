817b63e3c0f1c9110a476c33ac6ae112ee0e8596
AbdellahBellakrim <bellakrim2032@gmail.com>
2025-09-25 22:30:27 +0100

    Refactor ft_tqdm function to improve progress display with percentage, progress bar, and counter

--- day00/ex08/Loading.py
+++ day00/ex08/Loading.py
@@ -2,9 +2,23 @@ from time import sleep
 from tqdm import tqdm
 
 
-def ft_tqdm(iterable):
-    for index, elem in enumerate(iterable):
-        print(f"\r{(int(float((index + 1) * 100) / len(iterable)))}% [] {int(float((index + 1)))}/{len(iterable)} [00:01<00:00, 191.61it/s]", end="")
+def percentage(part: int, whole: int) -> str:
+    return f"{(int(float((part * 100) / whole)))}%"
+
+
+def progress_counter(count: int, total: int) -> None:
+    return f"{int(float((count)))}/{total}"
+
+
+def progress_bar(part: int, whole: int, bar_length: int = 189) -> str:
+    filled_length = int(bar_length * part // whole)
+    bar = '█' * filled_length + ' ' * (bar_length - filled_length)
+    return f"|{bar}|"
+
+
+def ft_tqdm(lst: range):
+    for index, elem in enumerate(lst):
+        print(f"\r{percentage(index + 1, len(lst))}{progress_bar(index + 1, len(lst))} {progress_counter(index + 1, len(lst))}", end="")
         yield elem
 
 
@@ -12,14 +26,6 @@ for elem in ft_tqdm(range(333)):
     sleep(0.005)
 print()
 
-# for elem in tqdm(range(333)):
-#     sleep(0.005)
-# print()
-
-
-
-# Displays a progress bar that updates in real-time
-# Shows completion percentage
-# Displays elapsed time and estimated time remaining
-# Shows iterations per second rate
-# Updates dynamically without spamming the console
+for elem in tqdm(range(333)):
+    sleep(0.005)
+print()
